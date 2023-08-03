b7089868cc9c6a41e2064edff7bb21df7607c41f
wilsoncCheng <89733416+wilsoncCheng@users.noreply.github.com>
2023-08-03 22:44:27 +0800

    login page &register page finish

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom"
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage"
+import { Logo, FormRow } from "../components"
 const Login = () => {
     return (
-        <div>
-            <h1> Login Page</h1>
-            <Link to="/register">register</Link>
-        </div>
+        <Wrapper>
+            <form className="form">
+                <Logo />
+                <h4>login</h4>
+                <FormRow type='email' name='email' defaultValue='wilson@gmail.com'></FormRow>
+                <FormRow type='password' name='password' defaultValue='password'></FormRow>
+                <button type="submit" className="btn btn-block">submit</button>
+                <button type="button" className="btn btn-block">explore the app</button>
+            </form>
+        </Wrapper>
     )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,21 @@
-
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage"
+import { Logo, FormRow } from '../components'
 const Register = () => {
     return (
-        <div>
-            Register
-        </div>
+        <Wrapper>
+            <form className="form">
+                <Logo />
+                <h4>Register</h4>
+                <FormRow type='text' name='name' defaultValue='Wilson'></FormRow>
+                <FormRow type='text' name='lastName' labelText='Last Name' defaultValue='Cheng'></FormRow>
+                <FormRow type='text' name='location' defaultValue='earth'></FormRow>
+                <FormRow type='email' name='email' defaultValue='wilson@gmail.com'></FormRow>
+                <FormRow type='password' name='password' defaultValue='12345'></FormRow>
+                <button type="submit" className="btn btn-block">
+                    submit
+                </button>
+            </form>
+        </Wrapper>
     )
 }
 
